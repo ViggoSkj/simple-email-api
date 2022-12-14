b7b80ab8dbc686270b3a62cb38bdf9ad7c15c353
ViggoSkj <ViggoSkjonsberg@gmail.com>
2022-12-14 17:05:13 +0100

    bug fixes and response messages

--- index.js
+++ index.js
@@ -10,13 +10,20 @@ app.use(express.json())
 
 
 async function AuthMiddleware(req, res, next) {
-    const apiKey = req.body.api_secret
-    const valid = await auth.validAPIKey(apiKey)
+    const apiSecret = req.body.api_secret
+
+    if (typeof apiSecret == undefined) {
+        res.statusMessage = "Didn't set body parameter 'api_secret'."
+        res.sendStatus(403).end()
+    }
+
+    const valid = await auth.validAPIKey(apiSecret)
     if (valid) {
-        console.log("valid api key")
+        console.log("valid api api secret")
         next()
     } else {
-        console.log("tried to acces with invalid api key")
+        console.log("tried to acces with invalid api secret")
+        res.statusMessage = "Invalid api secret"
         res.sendStatus(403)
     }
 }
@@ -27,6 +34,20 @@ app.post("/", async (req, res) => {
     let to = req.body.to
     let html = req.body.html
     let subject = req.body.subject
+
+    if (typeof to === undefined) {
+        res.statusMessage = "Didn't set body parameter 'to'."
+        res.sendStatus(400).end()
+        return
+    } else if (typeof html === undefined) {
+        res.statusMessage = "Didn't set body parameter 'html'."
+        res.sendStatus(400).end()
+        return
+    } else if (typeof subject === undefined) {
+        res.statusMessage = "Didn't set body parameter 'subject'."
+        res.sendStatus(400).end()
+        return
+    }
 
     await email.SendMail([to], subject, "", html)
     res.sendStatus(200)
